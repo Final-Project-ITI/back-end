f77dd5473ce2424c6bdbcccd4b4bdd7c63757403
abrarhussien <abrarhussien@gmail.com>
2024-05-06 18:19:29 +0200

    connect database

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const database = require("./database/database");
+
+
+database();
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
