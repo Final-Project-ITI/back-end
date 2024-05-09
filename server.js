30964912cc04b4dcbcaf5cf6b891e73e48cb47d1
Raafat1Heiba <raafatheiba6@gmail.com>
2024-05-09 05:04:00 +0300

    feat:add controller & service layers for the products endpoints

--- server.js
+++ server.js
@@ -11,31 +11,36 @@ database();
 
 const cartRouter = require("./routes/cart.js");
 const restaurantRouter = require("./routes/restaurant.js");
-const orderRouter = require('./routes/orders.router');
+const orderRouter = require("./routes/orders.router.js");
+const productRouter = require("./routes/product.router.js");
 
 const CartService = require("./services/cart.js");
 const RestaurantService = require("./services/restaurant.js");
-const OrderService = require('./services/orders.service');
+const OrderService = require("./services/orders.service");
+const ProductService = require("./services/product.service");
 
 const restaurantService = new RestaurantService();
 const cartService = new CartService();
 const orderService = new OrderService();
+const productService = new ProductService();
 
 const RestaurantController = require("./controllers/restaurant.js");
 const CartController = require("./controllers/cart.js");
-const OrderController = require('./controllers/orders.controller');
-
+const OrderController = require("./controllers/orders.controller");
+const ProductController = require("./controllers/product.controller");
 
 const restaurantController = new RestaurantController(restaurantService);
 const cartController = new CartController(cartService);
 const orderController = new OrderController(orderService);
+const productController = new ProductController(productService);
 
-mainRouter.use("/api/v1/cart", cartRouter(cartController));
-mainRouter.use("/api/v1/restaurant", restaurantRouter(restaurantController));
-mainRouter.use('/orders', orderRouter(orderController(orderService)));
+mainRouter.use("/cart", cartRouter(cartController));
+mainRouter.use("/restaurant", restaurantRouter(restaurantController));
+mainRouter.use("/orders", orderRouter(orderController));
+mainRouter.use("/products", productRouter(productController));
 
-app.use("/", mainRouter);
+app.use("/api/v1", mainRouter);
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
