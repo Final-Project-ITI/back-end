55926940e93d6986397887e5de03a63a17a407f5
waleedHAlmenawy <waleed.almenawy@outlook.com>
2024-05-10 02:36:18 +0300

    Implemented authentication login and middleware for normal user

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const authRouter = (authController) => {
-  router.get("/", (req, res) => {
-    const respones = authController.getHelloWorld();
-    res.send(respones);
+const asyncHandler = require('express-async-handler')
+
+const authRouter = (authController, authMiddleware, authService) => {
+  router.post("/login", asyncHandler(
+    async (req, res) => {
+      const respones = await authController.login(req.body);
+
+      res.status(respones.statusCode).send(respones.data);
+    }
+  ));
+
+
+  router.get("/test", authMiddleware.user(authService), (req, res) => {
+    console.log(req.auth);
+
+    res.send("Test Middleware");
   });
+
   return router;
 };
 module.exports = authRouter;
--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require("express");
-const authRouter = require("./routes/auth");
-const AuthController = require("./controllers/authCoteoller");
-const AuthService = require("./services/authService");
+const bodyParser = require('body-parser')
+
+const authRouter = require("./routes/auth.router");
+const AuthController = require("./controllers/auth.coteoller");
+const AuthService = require("./services/auth.service");
+const AuthMiddleware = require("./middlewares/auth.middleware");
+
+const mainRouter = express.Router();
 const app = express();
 const port = 3000;
+
 const database = require("./database/database");
+
 const authService = new AuthService();
 const authCoteoller = new AuthController(authService);
+const authMiddleware = new AuthMiddleware();
 
 database();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-app.use("/auth", authRouter(authCoteoller));
+mainRouter.use('/authentication', authRouter(authCoteoller, authMiddleware, authService));
+
+app.use('/api/v1', mainRouter);
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
